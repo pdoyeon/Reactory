22aec3dc80cb0e82c58bbde17c9407065f9b9df0
pdoyeon <pdoyeon06@gmail.com>
2025-07-11 20:35:48 +0900

    createElement 함수의 파라미터 유형

---
+++ src/chap03/createElement-parameter-test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+React.createElement(
+    // 엘리먼트의 유형
+    type,
+    // props
+    [props],
+    // 현재 엘리먼트가 포함하고 있는 자식 엘리먼트
+    [...children]
+)
